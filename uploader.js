be31db29901b18ea9b51e748d225fe5064b6e46d
Steven Isabelle <sisabelle@audyl.com>
2025-04-29 21:40:19 -0400

    log

--- uploader.js
+++ uploader.js
@@ -58,6 +58,7 @@ async function obtainFormTranscription(blob, formId, sessionId, token){
     });
     console.log("Calling waitForWebSocketMessageAfterPostxxxx");
     console.log(`${widgetConfig.websocketUrl}?sessionId=${sessionId}&role=${role}`);
+    console.log(queryParams.toString);
     console.log(`${widgetConfig.apiGatewayUploadUrl}?${queryParams.toString()}`)
     await waitForWebSocketMessageAfterPost(
         `${widgetConfig.websocketUrl}?sessionId=${sessionId}&role=${role}`,
